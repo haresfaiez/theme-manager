45e6a23dc9298eddad83f5e15ccb6ede0c1e645a
faiez <felhares@iobeya.com>
2020-11-14 17:23:18 +0100

    Remove unnecessary duplications

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -15,26 +15,15 @@ export default class Theme {
     return this.configurations[id].name;
   }
 
-  valueOf(id) {
-    return this.configurations[id].expression;
-  }
-
   updateExpression(id, newExpression) {
     this.configurations[id].expression = newExpression;
   }
 
   evaluatedValue(id) {
-    const expression = this.configurations[id].expression;
-
-    if (!expression) {
-      return this.valueOf(id);
-    }
-
-    return expression.replace(/{([^}]*)}/g, (id) => this.valueOf(id.replace(/({|})/g, '')));
+    return this.rawValue(id).replace(/{([^}]*)}/g, (id) => this.rawValue(id.replace(/({|})/g, '')));
   }
 
   rawValue(id) {
-    const expression = this.configurations[id].expression;
-    return expression || this.valueOf(id);
+    return this.configurations[id].expression;
   }
 }
