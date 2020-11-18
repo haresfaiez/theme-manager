1959d414a94853954a1fa2092df96c792518bf38
faiez <felhares@iobeya.com>
2020-11-18 22:17:23 +0100

    Add types validations

--- src/model/Theme.js
+++ src/model/Theme.js
@@ -42,8 +42,55 @@ class Theme {
     return this.configurations[categoryId].items[id].expression;
   }
 
-  validate(value, type) {
+  validateColor(value) {
+    if (!RegExp(/^#[a-fA-F0-9]{3}$/).test(value) && !RegExp(/^#[a-fA-F0-9]{6}$/).test(value)) {
+      return [`Failed to save color! "${value}" is not a valid hex code.`];
+    } else {
+      return [];
+    }
+  }
+
+  validateText(value) {
+    if (RegExp(/^\s*$/).test(value)) {
+      return ['Text value cannot contain only spaces!'];
+    } else {
+      return [];
+    }
+  }
+
+  validateNumber(rawValue) {
+    const value = Number(rawValue);
+    if(Number.isNaN(value)) {
+      return [`Failed to save value! "${rawValue}" is not a valid number.`];
+    }
+    if (value === Number.POSITIVE_INFINITY) {
+      return ['Failed to save value! Positive infenity is not a valid number.'];
+    }
+    if (value < 0) {
+      return [`Failed to save value! "${value}" is negative and a positive number is expected.`]
+    }
+
     return [];
+  }
+
+  validate(value, type) {
+    if (value === '' || value === null || value === undefined) {
+      return ['The value cannot be empty!'];
+    }
+
+    if (type === 'color') {
+      return this.validateColor(value);
+    }
+
+    if (type === 'text') {
+      return this.validateText(value);
+    }
+
+    if (['px', 'em', 'rem'].includes(type)) {
+      return this.validateNumber(value);
+    }
+
+    return [`Failed to recognize type "${type}"! Try to choose one of the offered options.`];
   }
 }
 
--- src/model/Theme.spec.js
+++ src/model/Theme.spec.js
@@ -23,4 +23,78 @@ describe('Theme', () => {
       expect(theme.getType('a', 'b')).toEqual('text');
     });
   });
+
+  describe('validation', () => {
+    it('rejects an empty value', () => {
+      const actual = new Theme().validate('', 'text');
+      expect(actual).toEqual(['The value cannot be empty!']);
+    });
+
+    it('rejects an non-recognized type', () => {
+      const actual = new Theme().validate('Y', 'random');
+      expect(actual).toEqual(['Failed to recognize type "random"! Try to choose one of the offered options.']);
+    });
+  });
+
+  describe('text validation', () => {
+
+    it('rejects a text value with only spaces', () => {
+      const actual = new Theme().validate('     ', 'text');
+      expect(actual).toEqual(['Text value cannot contain only spaces!']);
+    });
+  });
+
+  describe('color validation', () => {
+    it('accepts color uppercase hex code', () => {
+      const actual = new Theme().validate('#ABC1EB', 'color');
+      expect(actual).toEqual([]);
+    });
+
+    it('accepts color hex code', () => {
+      const actual = new Theme().validate('#e42fDe', 'color');
+      expect(actual).toEqual([]);
+    });
+
+    it('accepts color shortened hex code', () => {
+      const actual = new Theme().validate('#fff', 'color');
+      expect(actual).toEqual([]);
+    });
+
+    it('rejects non-hex color code', () => {
+      const actual = new Theme().validate('red', 'color');
+      expect(actual).toEqual(['Failed to save color! "red" is not a valid hex code.']);
+    });
+  });
+
+  describe('em/rem/px validation', () => {
+    it('rejects non-numbers', () => {
+      const actual = new Theme().validate('3aY7', 'px');
+      expect(actual).toEqual(['Failed to save value! "3aY7" is not a valid number.']);
+    });
+
+    it('rejects non-positive numbers', () => {
+      const actual = new Theme().validate('-7', 'em');
+      expect(actual).toEqual(['Failed to save value! "-7" is negative and a positive number is expected.']);
+    });
+
+    it('rejects positive infinity numberr', () => {
+      const actual = new Theme().validate(Number.POSITIVE_INFINITY, 'em');
+      expect(actual).toEqual(['Failed to save value! Positive infenity is not a valid number.']);
+    });
+
+    it('accepts zero', () => {
+      const actual = new Theme().validate('0', 'rem');
+      expect(actual).toEqual([]);
+    });
+
+    it('accepts a number', () => {
+      const actual = new Theme().validate(310, 'px');
+      expect(actual).toEqual([]);
+    });
+
+    it('accepts a number as a string', () => {
+      const actual = new Theme().validate('30', 'rem');
+      expect(actual).toEqual([]);
+    });
+  });
 });
